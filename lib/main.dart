dc7b0a52a474e659d6fefacfe975fc0fc7dbfc02
Zain Sajjad <zaingaming933@gmail.com>
2023-08-19 11:18:18 +0500

    this is the changes

--- lib/main.dart
+++ lib/main.dart
@@ -109,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              'hy this is new',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
